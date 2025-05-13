11f9d7db1beeaf4c12ae8c73e592527c20a42c4e
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-05-13 20:38:03 +0530

    Update mongoose dependency and refactor user model to post model

--- src/app.js
+++ src/app.js
@@ -1,68 +1,26 @@
-const express = require('express');
-const userModel = require("./models/user.model")
+const express = require("express")
+const postModel = require("./models/post.model")
 
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.post('/create-post', async (req, res) => {
 
+    const { imageUrl, caption } = req.body
 
-app.post('/create-user', async (req, res) => {
-
-    const user = await userModel.create({
-        username: req.body.username,
-        email: req.body.email,
-        age: req.body.age
+    const post = await postModel.create({
+        imageUrl,
+        caption
     })
 
     res.status(201).json({
-        message: "User created successfully",
-        user: user
+        message: "post created",
+        post
     })
 
-})
-
-
-app.get('/get-users', async (req, res) => {
-    const users = await userModel.findOne({
-        email: "test@test.com"
-    })
-
-    res.status(200).json({
-        message: "Users fetched successfully",
-        users: users
-    })
-})
-
-
-app.patch("/update-user", async (req, res) => {
-
-    await userModel.findOneAndUpdate({
-        username: "test_2"
-    }, {
-        email: "test@test.com"
-    })
-
-    res.status(200).json({
-        message: "User updated successfully"
-    })
-})
-
-app.delete('/delete-user', async (req, res) => {
-
-    await userModel.findOneAndDelete({
-        username: "test"
-    })
-
-    res.status(200).json({
-        message: "User deleted successfully"
-    })
 })
 
 
 module.exports = app
-
